b70d0711cd13ff32dd429a9a791ec08534967f8b
HelloSkills <HelloSkillsWeb@gmail.com>
2026-05-29 23:19:07 +0300

    refactor: добавляем request

--- src/httpClient/HTTPClient.ts
+++ src/httpClient/HTTPClient.ts
@@ -7,42 +7,27 @@ export class HTTPClient {
   static baseUrlAPI = 'http://localhost:3000'
 
   static async get<T>(endpoint: string): Promise<HTTPClientResponse<T>> {
-    const url = `${this.baseUrlAPI}${endpoint}`
-
-    try {
-      const result = await fetch(url)
-      const responseJSON = await result.json() as T
-
-      return {
-        data: responseJSON,
-        error: null
-      }
-    } catch (error: unknown) {
-      if (error instanceof Error) {
-        return {
-          data: null,
-          error: error.message
-        }
-      }
-
-      return {
-        data: null,
-        error: null
-      }
-    }
+    return this.request<T>(endpoint)
   }
 
   static async post<T>(endpoint: string, data: unknown): Promise<HTTPClientResponse<T>> {
+    return this.request<T>(endpoint, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+  }
+
+  private static async request<T>(
+    endpoint: string,
+    options?: RequestInit
+  ): Promise<HTTPClientResponse<T>> {
     const url = `${this.baseUrlAPI}${endpoint}`
 
     try {
-      const result = await fetch(url, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-      })
+      const result = await fetch(url, options)
       const responseJSON = await result.json() as T
 
       return {
